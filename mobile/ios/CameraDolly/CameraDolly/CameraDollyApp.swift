528317981f0199d5fa641562d5d3a8a0de008406
Kari Kolehmainen <kari.kolehmainen@vtt.fi>
2021-12-13 09:45:33 +0200

    added iOS app stub

---
+++ mobile/ios/CameraDolly/CameraDolly/CameraDollyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraDollyApp.swift
+//  CameraDolly
+//
+//  Created by Kari Kolehmainen on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CameraDollyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
